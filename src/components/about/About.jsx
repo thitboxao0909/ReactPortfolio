5e92e89b06d85a8d9f56096bfbdfb144f1904054
TBX <thitboxao0909@gmail.com>
2022-06-08 11:21:43 +1000

    added my project

---
+++ src/components/about/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './about.css'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Hi</h5>
+      <h2>Some about me</h2>
+      
+    </section>
+  )
+}
+
+export default About
