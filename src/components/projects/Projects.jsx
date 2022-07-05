d162c353e562b1256052a82f7b5ec5f5d7f63ae6
TBX <thitboxao0909@gmail.com>
2022-07-05 21:20:04 +1000

    projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './project.css'
+import IMG1 from '../../assets/projects1.jpg'
 
 const Projects = () => {
   return (
-    <section id='projects'>Projects</section>
+    <section id='projects'>
+      <h5>Recent Projects</h5>
+      <div className="containter projects__containter">
+        <article className='projects__item'>
+          <div className="projects__item-image"></div>
+          <h2>Project title</h2>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          
+        </article>
+      </div>
+    </section>
   )
 }
 
