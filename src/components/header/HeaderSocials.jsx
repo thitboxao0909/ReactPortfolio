9c2457454cadfbae52bac67ae5739a35f40a60eb
thitboxao0909 <thitboxao0909@gmail.com>
2022-07-06 15:52:42 +1000

    project final

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {BsGithub} from 'react-icons/bs'
 function HeaderSocials() {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
-        <a href="https://github.com" target="_blank"><BsGithub /></a>
+        <a href="https://linkedin.com" target="_blank" rel='noreferrer'><BsLinkedin /></a>
+        <a href="https://github.com" target="_blank" rel='noreferrer'><BsGithub /></a>
     </div>
   )
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,18 +1,52 @@
 import React from 'react'
 import './project.css'
-import IMG1 from '../../assets/projects1.jpg'
+import IMG1 from '../../assets/projects1.png'
+import IMG2 from '../../assets/projects2.jpg'
+import IMG3 from '../../assets/projects3.png'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Extracting data from tables using YOLO5 and Tabula',
+    github: 'https://github.com/thitboxao0909/FlyingPenguins'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Native Android-Firebase app hub for renting rooms',
+    github: 'https://github.com/thitboxao0909/RoomControl'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Basic facial recognition with Eigenface technique',
+    github: 'https://github.com/thitboxao0909/Eigenface'
+  }
+]
+
 
 const Projects = () => {
   return (
     <section id='projects'>
       <h5>Recent Projects</h5>
-      <div className="containter projects__containter">
-        <article className='projects__item'>
-          <div className="projects__item-image"></div>
-          <h2>Project title</h2>
-          <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          
-        </article>
+
+      <div className='container projects__container'>
+        {
+          data.map(({id, image, title, github}) => {
+            return(
+              <article key={id} className='projects__items'>
+                <div className="projects__items-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h2>{title}</h2>
+                <div className='projects__items-cta'>
+                 <a href={github} className='btn' target='_blank' rel='noreferrer'>Github</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
