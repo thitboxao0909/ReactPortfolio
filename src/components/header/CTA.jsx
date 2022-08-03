29935b275ad724b2df2b6f2616cee7029b35ce0e
TBX <thitboxao0909@gmail.com>
2022-08-03 20:29:45 +1000

    fix cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import CV from '../../assets/CVfinal.pdf'
+
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn btn-primary'>Download CV</a>
+        <a  href='https://drive.google.com/file/d/1uCGQA0Qx8vV6TOr8_RTBYs2LgLbTOLD6/view?usp=sharing' 
+            target="_blank" rel='noreferrer'
+            className='btn btn-primary'>Download CV</a>
         <a href='#contact' className='btn btn-primary'>Contact Me</a>
     </div>
   )
