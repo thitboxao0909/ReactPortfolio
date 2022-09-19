ae638d9fc73b33952f5f18f341c72b32a67b3b81
TBX <thitboxao0909@gmail.com>
2022-09-19 09:42:59 +0700

    new cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a  href='https://drive.google.com/file/d/1uCGQA0Qx8vV6TOr8_RTBYs2LgLbTOLD6/view?usp=sharing' 
+        <a  href='https://drive.google.com/file/d/1RmREgvPUuETdWhY1W41km_bg5q3zNJ_i/view?usp=sharing' 
             target="_blank" rel='noreferrer'
             className='btn btn-primary'>Download CV</a>
         <a href='#contact' className='btn btn-primary'>Contact Me</a>
